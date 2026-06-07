433b9efc502cd72ede7d25fecc414fef7b99bed3
Mati. R <mnsr7548@gmail.com>
2026-06-06 22:08:27 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// URL del Web App de Google Apps Script.
-// Reemplazá este valor después de desplegar (ver tutorial).
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCSq_WejfyZ63pMaSRv-guw9A0dakiO1gmEHc4DLpApZODCzfWchL6laka1uXXuiG2hA/exec';
+window.APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyCSq_WejfyZ63pMaSRv-guw9A0dakiO1gmEHc4DLpApZODCzfWchL6laka1uXXuiG2hA/exec';
